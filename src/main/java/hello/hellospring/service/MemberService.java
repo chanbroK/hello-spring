877d80129f04c15d7b2afbf2847419c88d746b65
chanbroK <koc0819@gmail.com>
2022-04-04 17:36:26 +0900

    temp

---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,10 @@
+package hello.hellospring.service;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+
+public class MemberService {
+    private MemoryMemberRepository memberRepo = new MemoryMemberRepository();
+    memberRepo.
+
+}
