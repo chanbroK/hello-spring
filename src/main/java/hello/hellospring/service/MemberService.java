ead1fca32a93a667b8a7bff06ac71aae19241a4f
chanbroK <koc0819@gmail.com>
2022-04-04 23:41:21 +0900

    add member Service

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -1,10 +1,55 @@
 package hello.hellospring.service;
 
-import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
 
 public class MemberService {
     private MemoryMemberRepository memberRepo = new MemoryMemberRepository();
-    memberRepo.
 
+    /**
+     * 회원 가입
+     *
+     * @param member
+     * @return id
+     */
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+
+        memberRepo.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 같은 이름의 중복 회원 불가능 검사
+     *
+     * @param member
+     */
+    private void validateDuplicateMember(Member member) {
+        Optional<Member> result = memberRepo.findByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 이름입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     *
+     * @return all members list
+     */
+    public List<Member> findMembers() {
+        return memberRepo.findAll();
+    }
+
+    /**
+     * 특정 회원 조회
+     *
+     * @param memberId
+     * @return member
+     */
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepo.findById(memberId);
+    }
 }
